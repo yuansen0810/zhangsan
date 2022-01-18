16019d1e5e8820d053156659bedf7da6948f6a46
yuansen0810 <2291368524@qq.com>
2022-01-18 18:19:14 +0800

    第一次想GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,47 @@
+from unicodedata import name
+
+"""
+print("hello world") # 字符串
+print(2333) # 整数
+print(2.333) # 小数
+print(True) # 布尔值
+print(()) # 元组
+print([]) # 数组
+print({}) # 字典
+
+锄禾日当午
+汗滴禾下土
+
+print("哈哈",2333,2.333)
+print("哈哈"+"嘻嘻") # 字符串的拼接（同类型数据）
+print("哈哈哈"*100)
+print(((1+2)*100-9.9)/2)
+print(2<3)
+name = "张三" # 把张三这个值给了名字叫做a的这个变量（变量需要用小写字母表示）
+print(name)
+
+a = input("请输入：")
+print("input获取的内容:",a)
+"""
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容：",a+b)
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+# a = str(2333)
+# print(type(a))
+# a = "sffhjhqwgjhwksgwqkjhsklqshqskhdssnxsx  xxnbxdxqxbwjk"
+# print(len(a))
+a = input("请输入：")
+b = input("请输入：")
+print("两端字符串的长度是：", len(a)+len(b))
+
+
+
+
+
